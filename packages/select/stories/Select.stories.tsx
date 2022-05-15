41b43cd66324fb3e07f5c58779146b97a1f437de
rixcian <rostislav.kremecek@protonmail.com>
2022-05-15 10:06:20 +0200

    feat: setup base select component

--- packages/select/stories/Select.stories.tsx
+++ packages/select/stories/Select.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from '@chakra-ui/layout';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Select } from '../src';
@@ -6,6 +7,13 @@ import { Select } from '../src';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/Select',
+  decorators: [
+    (story: Function) => (
+      <Container maxWidth="400px" mt="40px">
+        {story()}
+      </Container>
+    ),
+  ],
   component: Select,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -14,7 +22,13 @@ export default {
 } as ComponentMeta<typeof Select>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
+const Template: ComponentStory<typeof Select> = (args) => (
+  <Select {...args}>
+    <option value="hello">Hello</option>
+    <option value="chello">Chello</option>
+    <option value="konichiwa">Konichiwa</option>
+  </Select>
+);
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
